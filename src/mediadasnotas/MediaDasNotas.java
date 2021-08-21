90b7963f3bd603bb0cc56af707530f180a473259
ProfJessica <jessica.sgarcez@lideres.alicerceedu.com.br>
2021-08-21 12:28:39 -0300

    Primeiro commit

---
+++ src/mediadasnotas/MediaDasNotas.java
@@ -0,0 +1,24 @@
+package mediadasnotas;
+
+
+public class MediaDasNotas {
+
+   
+    public static void main(String[] args) {
+        
+        //Crie um programa que declare 4 notas, calcule a média e exiba a saída de dados.
+        
+        float nota1 = 8;
+        float nota2 = 7;
+        float nota3 = 10;
+        float nota4 = 9;
+        
+        float média = (nota1 + nota2 + nota3 + nota4)/4;
+                
+        System.out.println(média);
+        
+  
+
+    }
+    
+}
